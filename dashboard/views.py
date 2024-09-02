57bfdb460f98e44c83b27bf942f9d0c184af5888
Priti <pritichaugule44@gmail.com>
2024-09-02 22:34:33 +0530

    added dashboard in templet

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request,'index.htm')
+def login (request):
+    return render(request,'login.html')
+def register (request):
+    return render(request,'register.html')
+
 
--- register/urls.py
+++ register/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('register/', views.register, name='home'),
-    path('login/', views.login, name='home'), 
+    path('login/', views.login, name='new'), 
       # Home page route
 
 ]
