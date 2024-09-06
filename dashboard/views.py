e0f204dafc582dcc3c8919f493fedf0c42cb13c5
Disha <shivdeeksha1@gmail.com>
2024-09-06 09:38:15 +0530

    everything done linking left

--- dashboard/views.py
+++ dashboard/views.py
@@ -28,3 +28,37 @@ def topic(request):
     return render(request, 'topic.html')
 def track(request):
     return render(request, 'track.html')
+def quiz(request):
+    return render(request, 'quiz.html')
+def books(request):
+    return render(request, 'books.html')
+
+def rhym(request):
+    return render(request, 'cargame.html')
+
+def video(request):
+    return render(request, 'video.html')
+
+def animalgame(request):
+    return render(request, 'animalgame.html')
+
+
+from django.conf import settings
+from django.http import FileResponse
+import os
+
+def download_mathematics(request):
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'assets/Part-1.pdf')
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='Part-1.pdf')
+
+def download_science(request):
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'assets/science.pdf')
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='science.pdf')
+
+def download_history(request):
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'assets/history.pdf')
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='history.pdf')
+
+def download_geography(request):
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'assets/geography.pdf')
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='geography.pdf')
